1d5b9bb920f35571fdc71e72a415b21aab8d4cdc
zeoly <zeoly100@163.com>
2018-06-13 16:08:58 +0800

    import material

--- kyo-material/src/app/app.module.ts
+++ kyo-material/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
