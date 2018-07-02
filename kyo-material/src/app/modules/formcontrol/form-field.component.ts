4d7926f2071790e8a6f09cc159d6d70d40277dab
zeoly <zeoly100@163.com>
2018-07-02 17:36:05 +0800

    form field

--- kyo-material/src/app/modules/formcontrol/form-field.component.ts
+++ kyo-material/src/app/modules/formcontrol/form-field.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        const isSubmitted = form && form.submitted;
+        return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    }
+}
 
 @Component({
     selector: 'app-formcontrol-form-field',
@@ -7,4 +16,22 @@ import { Component } from '@angular/core';
 })
 export class FormFieldComponent {
 
+    errMsgControl = new FormControl('', []);
+
+    emailFormControl = new FormControl('', [
+        Validators.required,
+        Validators.email,
+    ]);
+
+    selectedControl = new FormControl('valid', [
+        Validators.required,
+        Validators.pattern('valid'),
+    ]);
+
+    checkboxControl = new FormControl('', [
+        Validators.required,
+        Validators.email,
+    ]);
+
+    matcher = new MyErrorStateMatcher();
 }
